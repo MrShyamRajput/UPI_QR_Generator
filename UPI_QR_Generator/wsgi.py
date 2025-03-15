c0aa621a05101933a916e59a555b495e6cfbc2ad
MrShyamRajput <144553787+MrShyamRajput@users.noreply.github.com>
2025-03-15 19:16:56 +0530

    wsgi.py

--- UPI_QR_Generator/wsgi.py
+++ UPI_QR_Generator/wsgi.py
@@ -5,4 +5,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'UPI_QR_Generator.settings')
 
 application = get_wsgi_application()
-app=application
+
+app = application
