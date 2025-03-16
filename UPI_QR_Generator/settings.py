358eab8a5da0537097e2789d4af6068078dbd930
MrShyamRajput <144553787+MrShyamRajput@users.noreply.github.com>
2025-03-16 18:14:54 +0530

    settings.py

--- UPI_QR_Generator/settings.py
+++ UPI_QR_Generator/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-%mrc6ulih1jd*2*j$n!$*xgig*+_)7a(i)9zd)_7knx*s$h5il
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
